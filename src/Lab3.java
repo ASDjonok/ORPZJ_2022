90c2522d40bb292a9f300d9c0b82f60848cff1bf
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2022-10-11 15:25:59 +0300

    commit for prefix and postfix iteration [lab3]

--- src/Lab3.java
+++ src/Lab3.java
@@ -14,18 +14,27 @@ public class Lab3 {
             System.out.println(i + ": *");
         }
 
-        int i = 6;
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i + ": *");
+
+            System.out.println("i=" + i);
+            int j = ++i;
+            System.out.println("i=" + i);
+        }
+
+        /*int i = 6;
         System.out.println(i + 1);
         System.out.println(1 + i);
         int j = i++;
-        /*int j = i;
-        i += 1;*/
+        *//*int j = i;
+        i += 1;*//*
         System.out.println("i=" + i + ", j=" + j);
         int j2 = ++i;
         System.out.println("i=" + i + ", j2=" + j2);
 
         System.out.println(++i);
-        System.out.println(i++);
+        System.out.println(i++);*/
 
         System.out.println(s1 == s2);
         System.out.println(s1.equals(s2));
