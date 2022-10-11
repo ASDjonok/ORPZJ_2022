ca2f4e13d2d235000c722c1f88503e1d9402ea80
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2022-10-11 14:49:52 +0300

    commit for prefix and postfix iteration [lab3]

--- src/Lab3.java
+++ src/Lab3.java
@@ -4,6 +4,29 @@ public class Lab3 {
 
         String s1 = new String("ASD");
         String s2 = new String("ASD");
+        System.out.println(1 == 1);
+        int a = 1;
+        int b = 1;
+        System.out.println(a == b);
+        int c = 2;
+        System.out.println("a == --c: " + (a == --c));
+        for (int i = 0; i < 10; ++i) {
+            System.out.println(i + ": *");
+        }
+
+        int i = 6;
+        System.out.println(i + 1);
+        System.out.println(1 + i);
+        int j = i++;
+        /*int j = i;
+        i += 1;*/
+        System.out.println("i=" + i + ", j=" + j);
+        int j2 = ++i;
+        System.out.println("i=" + i + ", j2=" + j2);
+
+        System.out.println(++i);
+        System.out.println(i++);
+
         System.out.println(s1 == s2);
         System.out.println(s1.equals(s2));
 
